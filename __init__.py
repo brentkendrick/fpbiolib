7ec7821b8b0f760ad254196ec368a1831999cc2c
“brentkendrick” <brentkend@gmail.com>
2022-01-16 10:47:31 -0700

    init update

--- __init__.py
+++ __init__.py
@@ -3,3 +3,19 @@
 # base version number
 __version__ = "0.0.0"
 
+
+# # Get git commit as string to append to version
+# def get_git_revision_short_hash():
+#     return (
+#         subprocess.check_output(["git", "rev-parse", "--short", "HEAD"])
+#         .decode("ascii")
+#         .strip()
+#     )
+
+
+# # append git commit if possible
+# try:
+#     commit = get_git_revision_short_hash()
+#     __version__ += "-" + commit
+# except subprocess.CalledProcessError:
+#     pass
